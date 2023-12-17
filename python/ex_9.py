cc9a0ead63cd59b8204aa7597438acdcb7d88602
Richard <clonadodogogle@gmail.com>
2023-12-16 23:19:25 -0300

    working...

--- python/ex_9.py
+++ python/ex_9.py
@@ -1,14 +1,15 @@
 i = 1 #sobe até 9
 a = 1 #sobe muito
 
+#enquanto verdade escreva "i x a: ", (i * a) e some +1 a i até ser igual ou maior que 11
 while True:
     print(i, "x", a,": ",i * a)
     i += 1
-
+# se i for
     if i >= 11 and not a >= 11:
         a+= 1
         i = 1
-    
+# quando ambas chegam a 10 eles encerram a contagem
     elif i == 10 and a == 10:        
         break
 
